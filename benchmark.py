78d5fb99bbca36fac78aec81b0db18d6e68fc84b
gychant <gychant@qq.com>
2020-06-16 13:53:38 -0400

    add cycic

---
+++ benchmark.py
@@ -0,0 +1,63 @@
+
+import json
+
+class Benchmark(object):
+    def load_question_file(self, path):
+        raise NotImplementedError
+
+    def load_label_file(self, path):
+        raise NotImplementedError
+
+    def get_benchmark_id(self):
+        raise NotImplementedError
+
+    def get_questions(self):
+        raise NotImplementedError
+
+    def get_question_type(self, question):
+        raise NotImplementedError
+
+    def get_question_categories(self, question):
+        raise NotImplementedError
+
+    def get_question_id(self, question):
+        raise NotImplementedError
+
+    def get_question_set_id(self, question):
+        raise NotImplementedError
+
+    def get_question_text(self, question):
+        raise NotImplementedError
+
+    def get_question_concept(self, question):
+        raise NotImplementedError
+
+    def get_choices(self, question):
+        raise NotImplementedError
+
+    def get_correct_choice_label(self, question):
+        raise NotImplementedError
+
+    def convert(self):
+        data = []
+
+        for i, ques in enumerate(self.get_questions()):
+            ques_data = dict()
+            ques_data["benchmarkId"] = self.get_benchmark_id()
+            ques_data["id"] = self.get_question_id(ques)
+            ques_data["questionSetId"] = self.get_question_set_id(ques)
+            ques_data["questionType"] = self.get_question_type(ques)
+            ques_data["categories"] = self.get_question_categories(ques)
+            ques_data["text"] = self.get_question_text(ques)
+            ques_data["concept"] = self.get_question_concept(ques)
+            ques_data["choices"] = self.get_choices(ques)
+            ques_data["correctChoiceLabel"] = self.get_correct_choice_label(ques)
+            data.append(ques_data)
+        return data
+
+    def write_data_as_jsonl(self, data, output_path):
+        with open(output_path, "w") as f:
+            for ques in data:
+                f.write(json.dumps(ques))
+                f.write("\n")
+
---
+++ cycic.py
@@ -0,0 +1,90 @@
+
+import os
+import json
+from benchmark import Benchmark
+
+
+class CycIC(Benchmark):
+    def __init__(self, question_set_id):
+        super(CycIC, self).__init__()
+        self.question_set_id = question_set_id
+        self.benchmark_id = "cycic"
+        self.questions = []
+        self.labels = dict()
+
+    def load_question_file(self, path):
+        with open(path) as f:
+            for line in f:
+                self.questions.append(json.loads(line.strip()))
+
+    def load_label_file(self, path):
+        with open(path) as f:
+            for line in f:
+                json_data = json.loads(line.strip())
+                self.labels[json_data["guid"]] = str(json_data["correct_answer"])
+
+    def get_benchmark_id(self):
+        return self.benchmark_id
+
+    def get_questions(self):
+        return self.questions
+
+    def get_question_type(self, question):
+        return question["questionType"]
+
+    def get_question_categories(self, question):
+        return question["categories"]
+
+    def get_question_id(self, question):
+        return question["guid"]
+
+    def get_question_set_id(self, question):
+        return self.question_set_id
+
+    def get_question_text(self, question):
+        return question["question"]
+
+    def get_question_concept(self, question):
+        return ""
+
+    def get_correct_choice_label(self, question):
+        return self.labels[self.get_question_id(question)]
+
+    def get_choices(self, question):
+        choices = []
+        if self.get_question_type(question) == "multiple choice":
+            num_choices = 5
+        elif self.get_question_type(question) == "true/false":
+            num_choices = 2
+
+        for i in range(num_choices):
+            choices.append({
+                "label": str(i),
+                "text": question["answer_option{}".format(i)]
+            })
+        return choices
+
+
+if __name__ == "__main__":
+    output_dir = "converted/CycIC"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    benchmark = CycIC(question_set_id="train")
+    benchmark.load_question_file("data/CycIC/CycIC_training_questions.jsonl")
+    benchmark.load_label_file("data/CycIC/CycIC_training_labels.jsonl")
+    data = benchmark.convert()
+    benchmark.write_data_as_jsonl(data, os.path.join(output_dir, "CycIC_train.jsonl"))
+
+    benchmark = CycIC(question_set_id="dev")
+    benchmark.load_question_file("data/CycIC/CycIC_dev_questions.jsonl")
+    benchmark.load_label_file("data/CycIC/CycIC_dev_labels.jsonl")
+    data = benchmark.convert()
+    benchmark.write_data_as_jsonl(data, os.path.join(output_dir, "CycIC_dev.jsonl"))
+
+    benchmark = CycIC(question_set_id="test")
+    benchmark.load_question_file("data/CycIC/CycIC_test_questions.jsonl")
+    benchmark.load_label_file("data/CycIC/CycIC_test_labels.jsonl")
+    data = benchmark.convert()
+    benchmark.write_data_as_jsonl(data, os.path.join(output_dir, "CycIC_test.jsonl"))
+
