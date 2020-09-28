78f379f1403d2a6a09f4e8646910ced9fb8e1d59
Minor Gordon <gordom6@rpi.edu>
2020-09-28 14:02:55 -0400

    pipeline unit test

---
+++ tests/mcs_benchmark_data_test/pipelines/commonsense_qa/commonsense_qa_benchmark_pipeline_test.py
@@ -0,0 +1,21 @@
+from mcs_benchmark_data.models.benchmark import Benchmark
+from mcs_benchmark_data.models.benchmark_dataset import BenchmarkDataset
+from mcs_benchmark_data.pipelines.commonsense_qa.commonsense_qa_benchmark_pipeline import (
+    CommonsenseQaBenchmarkPipeline,
+)
+
+
+def test_extract_transform():
+    models = tuple(CommonsenseQaBenchmarkPipeline().extract_transform())
+    assert models
+
+    benchmark = [model for model in models if isinstance(models, Benchmark)]
+    assert benchmark
+    benchmark = benchmark[0]
+    assert benchmark.name == "CommonsenseQA"
+
+    datasets = [model for model in models if isinstance(model, BenchmarkDataset)]
+    assert len(datasets) == 3
+    assert any(dataset.name == "dev" for dataset in datasets)
+    assert any(dataset.name == "test" for dataset in datasets)
+    assert any(dataset.name == "train" for dataset in datasets)
