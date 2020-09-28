ad6f97a22e6d728707ee006040390c88d5a601e3
Minor Gordon <gordom6@rpi.edu>
2020-09-28 13:50:12 -0400

    deleting unneeded classes

--- mcs_benchmark_data/extractors/nop_extractor.py
+++
@@ -1,6 +0,0 @@
-from mcs_benchmark_data._extractor import _Extractor
-
-
-class NopExtractor(_Extractor):
-    def extract(self, **kwds):
-        return {}
--- mcs_benchmark_data/loaders/default_loader.py
+++ mcs_benchmark_data/loaders/default_loader.py
@@ -1,7 +1,5 @@
-from mcs_benchmark_data.loaders.composite_loader import CompositeLoader
 from mcs_benchmark_data.loaders.rdf_file_loader import RdfFileLoader
 
 
-class DefaultLoader(CompositeLoader):
-    def __init__(self, **kwds):
-        CompositeLoader.__init__(self, loaders=(RdfFileLoader(**kwds),), **kwds)
+class DefaultLoader(RdfFileLoader):
+    pass
