5fc2e98b9184388e661bcb81ac835119a897f296
Minor Gordon <gordom6@rpi.edu>
2020-10-06 18:05:37 -0400

    sketch of further tests

--- tests/mcs_benchmark_data_test/pipelines/commonsense_qa/commonsense_qa_pipeline_test.py
+++ tests/mcs_benchmark_data_test/pipelines/commonsense_qa/commonsense_qa_pipeline_test.py
@@ -1,3 +1,6 @@
+from rdflib import Graph
+from rdflib.compare import to_isomorphic
+
 from mcs_benchmark_data.path import DATA_DIR_PATH
 from mcs_benchmark_data.pipelines.commonsense_qa.commonsense_qa_benchmark_pipeline import (
     CommonsenseQaBenchmarkPipeline,
@@ -10,4 +13,24 @@ def test_extract_transform_load(tmp_path):
     assert loaded_data_dir_path.is_dir()
     rdf_file_path = loaded_data_dir_path / (CommonsenseQaBenchmarkPipeline.ID + ".ttl")
     assert rdf_file_path.is_file()
-    # TODO: load the RDF file into a Graph and test its contents
+
+    old_graph = Graph()
+    for suffix in ("dev", "test", "train"):
+        with open(
+            loaded_data_dir_path / f"{CommonsenseQaBenchmarkPipeline.ID}_{suffix}.jsonl"
+        ) as jsonl_file:
+            for json_line in jsonl_file:
+                old_graph.parse(json_line, format="json-ld")
+
+    new_graph = Graph()
+    new_graph.parse(
+        str(loaded_data_dir_path / f"{CommonsenseQaBenchmarkPipeline.ID}.ttl")
+    )
+
+    old_graph = to_isomorphic(old_graph)
+    new_graph = to_isomorphic(new_graph)
+
+    # See dump examples in https://rdflib.readthedocs.io/en/4.0/_modules/rdflib/compare.html
+    assert new_graph == old_graph
+
+    # More likely pick out a few resources and compare them
