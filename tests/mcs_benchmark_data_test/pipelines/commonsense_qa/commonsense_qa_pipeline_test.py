f4bf2fc412ae969a5ad33037b7fa6d24a886a78a
Minor Gordon <gordom6@rpi.edu>
2020-10-06 18:20:41 -0400

    skip empty lines

--- tests/mcs_benchmark_data_test/pipelines/commonsense_qa/commonsense_qa_pipeline_test.py
+++ tests/mcs_benchmark_data_test/pipelines/commonsense_qa/commonsense_qa_pipeline_test.py
@@ -20,6 +20,8 @@ def test_extract_transform_load(tmp_path):
             loaded_data_dir_path / f"{CommonsenseQaBenchmarkPipeline.ID}_{suffix}.jsonl"
         ) as jsonl_file:
             for json_line in jsonl_file:
+                if not json_line.strip():
+                    continue
                 old_graph.parse(data=json_line, format="json-ld")
 
     new_graph = Graph()
